0bce1bd468d2c9481de29884439895c09c859445
Lars Jansøn Engvik <lars@engvik.nu>
2017-10-21 18:06:34 +0200

    express-graphql example (#1)
    * express-graphql example
    
    * Add newline
    
    * Fix indentation
    
    * Fix indentation

---
+++ express-graphql/Topping.js
@@ -0,0 +1,55 @@
+import createId from './create-id';
+
+// Database mock
+const toppingDB = {};
+
+// Topping
+export default class Topping {
+  constructor(id, { name }) {
+    this.id = id;
+    this.name = name;
+  }
+
+  updateTopping({ name }) {
+    this.name = name;
+  }
+}
+
+// Resolver
+const toppingResolver = {
+  getTopping: ({ id }) => {
+    if (!toppingDB[id]) {
+      throw new Error('No topping with id: ' + id);
+    }
+
+    return toppingDB[id];
+
+  },
+  createTopping: ({ input }) => {
+    const id = createId();
+
+    toppingDB[id] = new Topping(id, input);
+
+    return toppingDB[id];
+  },
+  updateTopping: ({ id, input }) => {
+    if (!toppingDB[id]) {
+      throw new Error('No topping with id: ' + id);
+    }
+
+    toppingDB[id].updateTopping(input);
+
+    return toppingDB[id];
+  },
+  deleteTopping: ({ id }) => {
+    if (!toppingDB[id]) {
+      throw new Error('No topping with id: ' + id);
+    }
+
+    delete toppingDB[id];
+
+    return true;
+  }
+}
+
+export { toppingResolver, toppingDB };
